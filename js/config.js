78a834a44127d5c26de30d2cbec9f25fbd9e10df
Eghosa Ordia <eghordia130@gmail.com>
2023-08-03 04:54:29 +0100

    swiper add

--- js/config.js
+++ js/config.js
@@ -3,11 +3,13 @@ tailwind.config = {
     extend: {
       colors: {
         primaryBlack: "rgb(28, 32, 35)",
+        secBlack: "#1b2023",
         primaryYellow: "rgb(248, 164, 26)",
         secondaryYellow: "rgb(245, 131, 7)",
         terYellow: "#f8bd11",
         quadYellow: "#f78308",
         White40: "#ffffffa6",
+
       },
     },
   },
